304d612457e53214d6435bd07b51ad448d2e535e
Feyisayo Sonubi <feyisayo.sonubi@outlook.com>
2016-03-27 21:19:52 +0100

    Finish up layout of Health Data Activity

--- app/src/main/java/ninja/feyisayo/apps/weightpal/CalorieActivity.java
+++ app/src/main/java/ninja/feyisayo/apps/weightpal/CalorieActivity.java
@@ -1,5 +1,10 @@
 package ninja.feyisayo.apps.weightpal;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +12,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.context.IconicsLayoutInflater;
 
 public class CalorieActivity extends AppCompatActivity {
 
@@ -20,19 +28,14 @@ public class CalorieActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calorie);
 
         ButterKnife.inject(this);
-
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setTitle("Add Food");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        provideToolbarEnhancement();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
 
         switch (item.getItemId()){
-            case R.id.home:
+            case android.R.id.home:
                 this.finish();
                 return true;
 
@@ -42,5 +45,13 @@ public class CalorieActivity extends AppCompatActivity {
 
     }
 
+    public void provideToolbarEnhancement(){
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Add Health Data");
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
 }
---
+++ app/src/main/java/ninja/feyisayo/apps/weightpal/CalorieSchema.java
@@ -0,0 +1,9 @@
+package ninja.feyisayo.apps.weightpal;
+
+/**
+ * Created by Feyisayo on 27/03/2016.
+ */
+public class CalorieSchema {
+
+
+}
